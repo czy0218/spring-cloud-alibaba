55e6aab73e1aa5e07d9a0b6e52549b7d0427c29a
czy0218 <czy0218@163.com>
2019-01-15 18:57:35 +0800

    update-nocas环境配置及使用多环境配置

--- hello-spring-cloud-alibaba-nacos-provider/src/main/java/com/czy/hello/spring/cloud/alibaba/nacos/provider/controller/EchoProviderController.java
+++ hello-spring-cloud-alibaba-nacos-provider/src/main/java/com/czy/hello/spring/cloud/alibaba/nacos/provider/controller/EchoProviderController.java
@@ -1,6 +1,8 @@
 package com.czy.hello.spring.cloud.alibaba.nacos.provider.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,9 +15,17 @@ public class EchoProviderController {
 
     @Value("${server.port}")
     private String port;
+    @Autowired
+    private ConfigurableApplicationContext configurableApplicationContext;
 
     @GetMapping(value = "/echo/{message}")
     public String echo(@PathVariable String message) {
         return "Hello Nacos Discovery " + message + " i am from port " + port;
+    }
+
+    @GetMapping(value = "/hi")
+    public String hi() {
+        String property = configurableApplicationContext.getEnvironment().getProperty("user.name");
+        return property;
     }
 }
